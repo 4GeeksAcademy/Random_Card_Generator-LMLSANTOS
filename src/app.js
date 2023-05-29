c0e63c09c7406e9c1ee879c732a1d50b2b3382d7
lmlsantos <126293567+lmlsantos@users.noreply.github.com>
2023-05-29 18:56:30 +0000

    Random Card Generator LMLSANTOS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,66 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  flipCard();
+  btnCard.onclick = flipCard;
 };
+
+// variables
+let btnCard = document.querySelector("#new-card");
+let topNumber = document.querySelector(".top-number");
+let bottomNumber = document.querySelector(".bottom-number");
+let suitDeck = document.querySelector(".suit");
+suitDeck.style.color = "purple";
+
+// variable for flipping the card
+let cardFlip = document.querySelector(".card");
+
+// function that flips the card
+const flipCard = () => {
+  cardFlip.classList.toggle("flipped");
+
+  let newNumber = generateNumber();
+  topNumber.innerText = newNumber;
+  bottomNumber.innerText = newNumber;
+  generateCard();
+};
+// function that generates a number
+const generateNumber = () => {
+  const numbers = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2"
+  ];
+
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+// This function generates the suit and color
+const generateCard = () => {
+  const suits = ["♦", "♣", "♠", "♥"];
+
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  const updateCard = suits[indexSuits];
+
+  if (updateCard === "♠" || updateCard === "♣") {
+    suitDeck.style.color = "black";
+  } else {
+    suitDeck.style.color = "red";
+  }
+
+  suitDeck.innerHTML = updateCard;
+};
+
+console.log("this is fun or not");
